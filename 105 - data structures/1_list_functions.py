a5ec505266c5bfff08011b9a827f229722901f49
giavile <99559679+giavile@users.noreply.github.com>
2023-11-09 00:25:06 -0800

    started 105 - data structures
    logging off for the night - giving myself a reminder of where i left off when i pick this up again

---
+++ 105 - data structures/1_list_functions.py
@@ -0,0 +1,5 @@
+# now we're on data structures
+# welcome to hell
+# i might use both list and array in this doc since it's gonna be a long one
+# for all intents and purposes, theyre the same in this scenario
+# i learned how to do all this in java leave me alone
